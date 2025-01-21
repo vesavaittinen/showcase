38ba1af17829502c18ac8948cec47f21a24b1ce2
vesavaittinen <156056660+vesavaittinen@users.noreply.github.com>
2025-01-21 14:47:25 +0200

    Helloworld updated

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {RouterModule} from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  imports: [RouterModule, MatButtonModule],
+  imports: [RouterModule, MatButtonModule, MatCardModule ],
   templateUrl: './hello-world.component.html',
   styleUrl: './hello-world.component.css'
 })
