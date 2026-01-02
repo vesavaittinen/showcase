55f6e13c912c32ce6202a10aa70520296f8ebe59
Vesa Vaittinen <156056660+vesavaittinen@users.noreply.github.com>
2026-01-02 20:27:50 +0200

    Simplified data fetch service

--- src/app/services/fetchkinodata.service.ts
+++ src/app/services/fetchkinodata.service.ts
@@ -13,20 +13,9 @@ export class FetchkinodataService {
   url: any;
   
   constructor() {
-    this.url = 'https://www.finnkino.fi/xml/News/';
+    this.url = 'https://api.chucknorris.io/jokes/random';
   }
   getFinnkinoData(): Observable<any> {
-      // pipe combines all operators (map, filter, etc)
-      // map operator to change the type (for example: response -> JSON type)
-      // filter operator filters according to a condition
-      return this.http.get(this.url, { responseType: 'text' }).pipe(map(response => {
-      let newsData: any;
-      parseString(response, { trim: true, explicitArray: false, mergeAttrs: true},
-        function (err: any, result: any) {
-          newsData = result.News.NewsArticle;
-        }
-      );
-      return newsData;
-      }));
+      return this.http.get<any[]>(this.url);
   }
 }
