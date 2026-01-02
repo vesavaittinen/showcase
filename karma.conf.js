1163b253dd0b00a9609b59af30be3d29306a9499
Vesa Vaittinen <156056660+vesavaittinen@users.noreply.github.com>
2026-01-02 12:14:43 +0200

    Install karma-firefox-launcher plugin

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-example'),
+      dir: require('path').join(__dirname, './coverage/showcase'),
       subdir: '.',
       reporters: [
         { type: 'html' },
