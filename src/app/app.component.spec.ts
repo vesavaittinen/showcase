7b9f389a9b71537ee023dc6119fde500a8229881
Vesa Vaittinen <156056660+vesavaittinen@users.noreply.github.com>
2026-01-09 14:12:14 +0200

    Add routes to testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,14 @@ import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RouterTestingHarness } from '@angular/router/testing';
+import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [ provideRouter([{path: '/', component: AppComponent}])],
+      providers: [ provideRouter(routes)],
     }).compileComponents();
   });
 
